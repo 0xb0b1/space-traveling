487d2b9e8d81c62bb4819748827ece323be86e7f
0xb0b1 <vcente82@gmail.com>
2021-05-27 18:48:48 -0300

    ...

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
@@ -16,6 +14,11 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script
+            async
+            defer
+            src="https://static.cdn.prismic.io/prismic.js?new=true&repo=space-traveling-paulo-vicente"
+          />
         </body>
       </Html>
     );
