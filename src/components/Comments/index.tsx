552abc910169617435b682faa9c72430e6c5331f
Paulo Vicente <vcente82@gmail.com>
2021-05-27 23:39:46 -0300

    Update index.tsx

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -10,7 +10,7 @@ export default class Comments extends Component {
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('crossorigin', 'anonymous');
     script.setAttribute('async', 'true');
-    script.setAttribute('repo', 'GBDev13/space-traveling');
+    script.setAttribute('repo', '0xb0b1/space-traveling');
     script.setAttribute('issue-term', 'pathname');
     script.setAttribute('theme', 'dark-blue');
     anchor.appendChild(script);
